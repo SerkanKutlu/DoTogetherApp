aa680b865258561cbab864b52b4b9f36f4ba61c9
Serkan Kutlu <1serkankutlu@gmail.com>
2023-07-29 17:12:31 +0300

    running on both devices

--- App.tsx
+++ App.tsx
@@ -1,27 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <View>
-        <Text>Serkan</Text>
+        <Text>Serkan Hem iphone hem android</Text>
       </View>
     </SafeAreaView>
   );
