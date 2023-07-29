e1bff6cbb03ba9bcfdd7df9cb02f37c15a56f232
Serkan Kutlu <1serkankutlu@gmail.com>
2023-07-29 16:46:30 +0300

    project created

---
+++ App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Serkan</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
