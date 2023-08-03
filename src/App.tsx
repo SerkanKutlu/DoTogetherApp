3d19ae0832f1d31c15f9bf9d1732ce45f93931bb
Serkan Kutlu <1serkankutlu@gmail.com>
2023-08-03 21:34:36 +0300

    working with native-paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 //DoTogether!
 import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {Button} from 'react-native-paper';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <Text>Serkan</Text>
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </SafeAreaView>
   );
 }
