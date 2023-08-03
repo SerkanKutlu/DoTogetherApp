9730e7492729930e17a716dd6da39191818d7ad4
Serkan Kutlu <1serkankutlu@gmail.com>
2023-08-03 21:29:24 +0300

    running on both with dummy screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 //DoTogether!
 import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
-import useStyles from './AppStyle';
-import OnBoard from './pages/OnBoard/OnBoard';
 function App(): JSX.Element {
-  const styles = useStyles();
   return (
-    <SafeAreaView style={styles.container}>
-      <OnBoard />
+    <SafeAreaView>
+      <Text>Serkan</Text>
     </SafeAreaView>
   );
 }
