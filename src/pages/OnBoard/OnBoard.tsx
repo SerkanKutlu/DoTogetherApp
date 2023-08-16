a6511fd195324ba48019092e39e0fae01c400f6c
Serkan Kutlu <1serkankutlu@gmail.com>
2023-08-16 17:12:57 +0300

    modal added

--- src/pages/OnBoard/OnBoard.tsx
+++ src/pages/OnBoard/OnBoard.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import {useState} from 'react';
 import {
   useWindowDimensions,
   TouchableOpacity,
   View,
   Text,
   ScrollView,
+  Modal,
 } from 'react-native';
-import {Button} from 'react-native-paper';
+import {Button, TextInput} from 'react-native-paper';
 import useStyles from './OnBoardStyle';
+import {transparent} from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 function OnBoard(): JSX.Element {
   const {width, height} = useWindowDimensions();
+  const [createRoomModalVisible, setCreateRoomModalVisible] = useState(false);
   const styles = useStyles();
+  const [roomNameInput, setroomNameInput] = React.useState('');
+  function CreateRoomButtonClicked() {
+    setCreateRoomModalVisible(true);
+  }
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={createRoomModalVisible}
+        onRequestClose={() => {
+          setCreateRoomModalVisible(!createRoomModalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+              label="Room Name"
+              value={roomNameInput}
+              onChangeText={text => setroomNameInput(text)}
+              placeholder="Room Name..."
+              style={styles.roomNameInput as any}
+            />
+            <Button
+              icon="plus"
+              mode="elevated"
+              style={styles.modalButton}
+              onPress={() => setCreateRoomModalVisible(false)}>
+              Create
+            </Button>
+          </View>
+        </View>
+      </Modal>
       <View style={styles.navbar}>
         <Button
           icon="account-multiple-plus"
@@ -25,8 +59,8 @@ function OnBoard(): JSX.Element {
         <Button
           icon="plus"
           mode="elevated"
-          onPress={() => console.log('Pressed')}>
-          Create Fuck Me
+          onPress={() => CreateRoomButtonClicked()}>
+          Create
         </Button>
       </View>
       <View style={styles.roomList}>
@@ -42,9 +76,7 @@ function OnBoard(): JSX.Element {
                 </Text>
               </View>
             </TouchableOpacity>
-            {/* <TouchableOpacity>
-              <Icon name="close" size={width * 0.07} color="#4A4A4A" />
-            </TouchableOpacity> */}
+            <TouchableOpacity></TouchableOpacity>
           </View>
         </ScrollView>
       </View>
--- src/pages/OnBoard/OnBoardStyle.js
+++ src/pages/OnBoard/OnBoardStyle.js
@@ -48,5 +48,33 @@ export default function useStyles() {
       height: '100%',
       justifyContent: 'center',
     },
+    centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 22,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    roomNameInput: {
+      width: '200@s',
+      height: '40@s',
+    },
+    modalButton: {
+      marginTop: '20@s',
+    },
   });
 }
