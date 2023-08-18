592bb59aff84477689286f7b5a5cb924d2cb637e
Serkan Kutlu <1serkankutlu@gmail.com>
2023-08-19 00:14:43 +0300

    no login screen shown when logging slightly

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,55 +8,65 @@ import {ActiveUser, AuthService} from '../../Services/AuthService';
 import {Platform} from 'react-native';
 function Login({navigation}): JSX.Element {
   const {width, height} = useWindowDimensions();
+  const [showLogin, setShowLogin] = useState(false);
   const authService = new AuthService();
   const styles = useStyles();
   console.log('xxxxx');
   useEffect(() => {
-    authService.LoginAgain().then(() => {
-      console.log('navigating bro');
-      navigation.navigate('OnBoard');
-    });
+    authService
+      .LoginAgain()
+      .then(() => {
+        console.log('navigating bro');
+        navigation.navigate('OnBoard');
+      })
+      .catch(() => {
+        setShowLogin(true);
+      });
   }, []);
   return (
-    <SafeAreaView style={styles.loginContainer}>
-      <View style={styles.headerArea}>
-        <Text style={styles.header as any}>Do Together</Text>
-      </View>
-      <View style={styles.loginOptionsContainer}>
-        <Button
-          style={styles.loginElment}
-          icon="google"
-          mode="elevated"
-          onPress={async () => {
-            try {
-              await authService.GoogleLogin();
-              navigation.navigate('OnBoard');
-            } catch {}
-          }}>
-          Continue With Google
-        </Button>
-        {Platform.OS === 'ios' && (
-          <Button
-            style={styles.loginElment}
-            icon="apple"
-            mode="elevated"
-            onPress={async () => await authService.AppleLogin()}>
-            Continue With Apple
-          </Button>
-        )}
+    <SafeAreaView>
+      {showLogin && (
+        <SafeAreaView style={styles.loginContainer}>
+          <View style={styles.headerArea}>
+            <Text style={styles.header as any}>Do Together</Text>
+          </View>
+          <View style={styles.loginOptionsContainer}>
+            <Button
+              style={styles.loginElment}
+              icon="google"
+              mode="elevated"
+              onPress={async () => {
+                try {
+                  await authService.GoogleLogin();
+                  navigation.navigate('OnBoard');
+                } catch {}
+              }}>
+              Continue With Google
+            </Button>
+            {Platform.OS === 'ios' && (
+              <Button
+                style={styles.loginElment}
+                icon="apple"
+                mode="elevated"
+                onPress={async () => await authService.AppleLogin()}>
+                Continue With Apple
+              </Button>
+            )}
 
-        <View style={[styles.orContainer, styles.loginElment]}>
-          <Text style={styles.or as any}>or</Text>
-        </View>
-        <Button
-          style={styles.loginElment}
-          mode="elevated"
-          onPress={() => {
-            console.log(ActiveUser.GetActiveUser());
-          }}>
-          Create Account
-        </Button>
-      </View>
+            <View style={[styles.orContainer, styles.loginElment]}>
+              <Text style={styles.or as any}>or</Text>
+            </View>
+            <Button
+              style={styles.loginElment}
+              mode="elevated"
+              onPress={() => {
+                console.log(ActiveUser.GetActiveUser());
+              }}>
+              Create Account
+            </Button>
+          </View>
+        </SafeAreaView>
+      )}
     </SafeAreaView>
   );
 }
